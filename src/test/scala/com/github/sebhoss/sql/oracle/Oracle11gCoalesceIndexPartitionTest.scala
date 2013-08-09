ce2f7461c38064029b0e3e4264da3cf2ddf1ad64
Sebastian Hoß <mail@shoss.de>
2013-08-09 02:28:09 +0200

    Added tests for coalesce index partition clause
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ src/test/scala/com/github/sebhoss/sql/oracle/Oracle11gCoalesceIndexPartitionTest.scala
@@ -0,0 +1,22 @@
+package com.github.sebhoss.sql.oracle
+
+import com.github.sebhoss.sql.common.StringHelper._
+
+class Oracle11gCoalesceIndexPartitionTest extends Oracle11gTest {
+  
+  def rule = Oracle11gAlterIndexParser.coalesce_index_partition
+
+  def statements = Oracle11gCoalesceIndexPartitionTest.statements
+
+}
+
+object Oracle11gCoalesceIndexPartitionTest {
+
+  val statements = {
+    val coalesce = Set("COALESCE PARTITION")
+    val parallel = Set("") ++ Oracle11gParallelClauseTest.statements
+
+    cartesian(coalesce, parallel)
+  }
+
+}
