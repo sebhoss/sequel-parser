23818e468b73eb8a97c454725f2e7ad45a1ac4cb
Sebastian Hoß <mail@shoss.de>
2013-08-09 02:36:28 +0200

    Added tests for key compression clause
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ src/test/scala/com/github/sebhoss/sql/oracle/Oracle11gKeyCompressionTest.scala
@@ -0,0 +1,19 @@
+package com.github.sebhoss.sql.oracle
+
+import com.github.sebhoss.sql.common.StringHelper._
+
+class Oracle11gKeyCompressionTest extends Oracle11gTest {
+  
+  def rule = Oracle11gCreateIndexParser.key_compression
+
+  def statements = Oracle11gKeyCompressionTest.statements
+
+}
+
+object Oracle11gKeyCompressionTest {
+
+  val statements = {
+    cartesian(Set("COMPRESS"), Set("1", "10", "100")) ++ Set("NOCOMPRESS")
+  }
+
+}
