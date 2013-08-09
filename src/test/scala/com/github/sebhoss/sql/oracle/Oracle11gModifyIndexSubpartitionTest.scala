ee64b60311b49be4f096dacc9fbdfec946a1a168
Sebastian Hoß <mail@shoss.de>
2013-08-09 02:24:51 +0200

    Add tests for modify index subpartition clause
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ src/test/scala/com/github/sebhoss/sql/oracle/Oracle11gModifyIndexSubpartitionTest.scala
@@ -0,0 +1,22 @@
+package com.github.sebhoss.sql.oracle
+
+import com.github.sebhoss.sql.common.StringHelper._
+
+class Oracle11gModifyIndexSubpartitionTest extends Oracle11gTest {
+  
+  def rule = Oracle11gAlterIndexParser.modify_index_subpartition
+
+  def statements = Oracle11gModifyIndexSubpartitionTest.statements
+
+}
+
+object Oracle11gModifyIndexSubpartitionTest {
+
+  val statements = {
+    val modify = Set("MODIFY SUBPARTITION subpartition")
+    val partition = Set("UNUSABLE") ++ Oracle11gAllocateExtentClauseTest.statements ++ Oracle11gDeallocateUnusedClauseTest.statements
+
+    cartesian(modify, partition)
+  }
+
+}
