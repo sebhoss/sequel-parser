0f17e87a2398af2a6ed7df94abcb8d41ba9617c3
Sebastian Hoß <mail@shoss.de>
2013-09-09 02:28:59 +0200

    Deactivated debugging-test for surefire-run
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/test/scala/com/github/sebhoss/sql/oracle/Oracle11gDebuggingTest.scala
+++ src/test/scala/com/github/sebhoss/sql/oracle/Oracle11gDebuggingTest.scala
@@ -1,11 +1,15 @@
 package com.github.sebhoss.sql.oracle
 
 import scala.language.postfixOps
+import org.junit.runner.RunWith
+import org.parboiled.errors.ParseError
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
 import com.github.sebhoss.sql.SQLParser.Oracle
 import org.parboiled.scala.parserunners.TracingParseRunner
 import org.parboiled.support.ParseTreeUtils
 
-class Oracle11gDebuggingTest extends Oracle11gTest {
+class Oracle11gDebuggingTest extends FunSuite {
 
   def statements = List(
     """
@@ -15,22 +19,25 @@ class Oracle11gDebuggingTest extends Oracle11gTest {
 
   def rule = Oracle11gCreateIndexParser.add_column_clause
 
-    for ((statement) ← statements) {
-      test(s"The statement [$statement] should be matched be the parser!") {
-        // Given
-        val runner = TracingParseRunner(rule)
+  for ((statement) ← statements) {
+//    test(s"The statement [$statement] should be matched be the parser!") {
+//      // Given
+//      val runner = TracingParseRunner(rule)
+//
+//      // When
+//      val result = runner.run(statement)
+//
+//      // Then
+//      assert(result.matched, "Could not parse:\n\n\t" + generateErrorMessage(result.parseErrors, statement) +
+//          "\nTree: " + ParseTreeUtils.printNodeTree(result));
+//    }
+  }
   
-        //      .filter(
-        //        Lines(10 until 20) && Rules.below(parser.Factor) && !Rules.below(parser.Digits)
-        //      )
-  
-        // When
-        val result = runner.run(statement)
-  
-        // Then
-        assert(result.matched, "Could not parse:\n\n\t" + generateErrorMessage(result.parseErrors, statement) +
-          "\nTree: " + ParseTreeUtils.printNodeTree(result));
-      }
-    }
+  def generateErrorMessage(errors : List[ParseError], statement : String) = {
+    errors map (error ⇒
+      statement.slice(0, error.getStartIndex()) + "[" +
+        statement.slice(error.getStartIndex(), error.getEndIndex()) + "]" +
+        statement.slice(error.getEndIndex(), statement.length())) mkString("\n")
+  }
 
 }
