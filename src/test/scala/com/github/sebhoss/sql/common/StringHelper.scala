55934e145e3566f0bec95a15f6fba7fbfa330974
Sebastian Hoß <mail@shoss.de>
2013-09-09 20:40:04 +0200

    Removed redundant functions
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/test/scala/com/github/sebhoss/sql/common/StringHelper.scala
+++ src/test/scala/com/github/sebhoss/sql/common/StringHelper.scala
@@ -8,17 +8,11 @@ object StringHelper {
   def powerset(elements: String*) =
     flatten(elements.toSet.subsets.toSet)
 
-//  def combine(f: (String, String) => String)(xs: Traversable[Iterator[String]]) =
-//   xs reduceLeft { (x, y) => for (a <- x; b <- y) yield f(a, b) }
-//
-//  def cartesian(xs: Iterator[String]*) = 
-//    combine(_.trim + " " + _.trim)(xs)
-    
   def combination(xx: List[List[String]], i: Int): String = xx match {
     case Nil => ""
     case x :: xs => x(i % x.length).trim + " " + combination(xs, i/x.length).trim
   }
-  
+
   def cartesian(ll: List[String]*): Iterator[String] = {
     Iterator.from(0).takeWhile(n => n < ll.map(_.length).product).map(combination(ll.toList, _))
   }
