fc8e6cc04b63d6069b2fac6455bdd7c7857f07a0
Sebastian Hoß <mail@shoss.de>
2013-09-10 13:15:52 +0200

    Added missing documentation
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/test/scala/com/github/sebhoss/sql/common/SingleParserRuleTest.scala
+++ src/test/scala/com/github/sebhoss/sql/common/SingleParserRuleTest.scala
@@ -7,11 +7,19 @@ import org.scalatest.junit.JUnitRunner
 import org.parboiled.scala.rules.Rule0
 import org.parboiled.scala.parserunners.ReportingParseRunner
 
+/**
+ * Checks a single SQL statement against a single parsing rule.
+ */
 @RunWith(classOf[JUnitRunner])
 abstract class SingleParserRuleTest extends FunSuite {
 
+  /** The name of the database the given statement should be validated against. */
   def databaseName: String
+  
+  /** The parsing rule to use. */
   def rule: Rule0
+  
+  /** Statements to check against the above parsing rule. */
   def statements: TraversableOnce[String]
 
   for (statement ← statements) {
@@ -27,6 +35,9 @@ abstract class SingleParserRuleTest extends FunSuite {
     }
   }
 
+  /**
+   * Marks the parsing errors inside the given statement with enclosing brackets.
+   */
   def generateErrorMessage(errors : List[ParseError], statement : String) = {
     errors map (error ⇒
       statement.slice(0, error.getStartIndex()) + "[" +
