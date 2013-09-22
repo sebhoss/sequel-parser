df525ca893cd124c896327d4f053fd894fc33563
Sebastian Hoß <mail@shoss.de>
2013-09-22 20:14:45 +0200

    Add test case for 'varray_storage_clause' rule
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ src/test/scala/com/github/sebhoss/sql/oracle/Oracle11gVarrayStorageClauseTest.scala
@@ -0,0 +1,25 @@
+package com.github.sebhoss.sql.oracle
+
+import com.github.sebhoss.sql.common.StringHelper._
+
+class Oracle11gVarrayStorageClauseTest extends Oracle11gTest {
+
+  def rule = Oracle11gCreateTableParser.varray_storage_clause
+
+  def statements = Oracle11gVarrayStorageClauseTest.statements
+
+}
+
+object Oracle11gVarrayStorageClauseTest {
+
+  val statements = {
+    val store = Set("STORE AS")
+    val where = Set("SECUREFILE", "BASICFILE")
+    val lob = Set("LOB")
+    val params = cartesian(Set("", "segname"), Set("("), Oracle11gLOBStorageParameters.statements.take(1), Set(")"))
+    val segname = Set("segname")
+    
+    cartesian(cartesian(store, where, lob), params, segname)
+  }
+
+}
